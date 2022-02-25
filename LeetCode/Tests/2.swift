a4d60f7d65ee76a7f99d926704bd779c6e645be8
gaoyu <gaoyuexit@163.com>
2022-02-25 19:13:40 +0800

    update 2. 两数相加[虚拟头结点]

--- LeetCode/Tests/2.swift
+++ LeetCode/Tests/2.swift
@@ -9,6 +9,9 @@
 import XCTest
 
 /**
+ 2. 两数相加
+ 链接：https://leetcode-cn.com/problems/add-two-numbers
+ 
  给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
  请你将两个数相加，并以相同形式返回一个表示和的链表。
  你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
@@ -31,14 +34,9 @@ import XCTest
  每个链表中的节点数在范围 [1, 100] 内
  0 <= Node.val <= 9
  题目数据保证列表表示的数字不含前导零
-
- 来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/add-two-numbers
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 class TwoAdd {
-    
     //1: 递归实现
     class func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         return _addTwoNumbers(l1, l2, carry: 0)
@@ -85,36 +83,86 @@ class TwoAdd {
     }
 }
 
-class TwoAddTest: XCTestCase {
+//推荐->虚拟头结点
+//示例 1：
+//输入：l1 = [2,4,3],
+//     l2 = [5,6,4,5]
 
+//输出：     [7,0,8,5]
+//解释：342 + 465 = 807.
+//
+//示例 2：
+//输入：l1 = [0], l2 = [0]
+//输出：[0]
+//
+//示例 3：
+//输入：l1 = [9,9,9,9,9,9,9],
+//     l2 = [9,9,9,9]
+//输出：     [8,9,9,9,0,0,0,1]
+class _2: XCTestCase {
     
-    
-    func initNode() -> (p1: ListNode, p2: ListNode) {
-        let pL1 = ListNode(2)
-        let pL2 = ListNode(4)
-        let pL3 = ListNode(3)
-        pL1.next = pL2
-        pL2.next = pL3
-        pL3.next = nil
-
-        let qL1 = ListNode(5)
-        let qL2 = ListNode(6)
-        let qL3 = ListNode(4)
-        qL1.next = qL2
-        qL2.next = qL3
-        qL3.next = nil
-        return (pL1, qL1)
+    class Solution {
+        func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+            if l1 == nil { return nil }
+            if l2 == nil { return nil }
+            //虚拟头结点
+            let dummy = ListNode(0)
+            //进位
+            var carry = 0
+            var l1 = l1
+            var l2 = l2
+            var preNode = dummy
+            while l1 != nil || l2 != nil {
+                let sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry
+                let node = ListNode(sum % 10)
+                carry = sum / 10
+                preNode.next = node
+                preNode = node
+                l1 = l1?.next
+                l2 = l2?.next
+            }
+            //检查carry
+            if carry > 0 { preNode.next = ListNode(carry) }
+            return dummy.next
+        }
     }
     
     func testTwoAdd() {
-        let result = TwoAdd.addTwoNumbers(initNode().p1, initNode().p2)
-        print(result!)
-        XCTAssert(result!.debugDescription == "7 0 8", "case error")
+        let head1 = ListNode.create([2,4,3])
+        let head2 = ListNode.create([5,6,4])
+        let result = Solution().addTwoNumbers(head1, head2)
+        XCTAssert(ListNode.print(result) == [7,0,8])
     }
     
     func testTwoAdd1() {
-        let result = TwoAdd.addTwoNumbers1(initNode().p1, initNode().p2)
-        print(result!)
-        XCTAssert(result!.debugDescription == "7 0 8", "case error")
+        let head1 = ListNode.create([2,4,3])
+        let head2 = ListNode.create([5,6,4])
+        let result = Solution().addTwoNumbers(head1, head2)
+        XCTAssert(ListNode.print(result) == [7,0,8])
+    }
+    
+    //最终解决方法
+    func test() {
+        example1()
+        example2()
+        example3()
+    }
+    func example1() {
+        let head1 = ListNode.create([2,4,3])
+        let head2 = ListNode.create([5,6,4])
+        let result = Solution().addTwoNumbers(head1, head2)
+        XCTAssert(ListNode.print(result) == [7,0,8])
+    }
+    func example2() {
+        let head1 = ListNode.create([0])
+        let head2 = ListNode.create([0])
+        let result = Solution().addTwoNumbers(head1, head2)
+        XCTAssert(ListNode.print(result) == [0])
+    }
+    func example3() {
+        let head1 = ListNode.create([9,9,9,9,9,9,9])
+        let head2 = ListNode.create([9,9,9,9])
+        let result = Solution().addTwoNumbers(head1, head2)
+        XCTAssert(ListNode.print(result) == [8,9,9,9,0,0,0,1])
     }
 }
