d99993a580b1e1926236577a751f03b45a20b906
gaoyu <gaoyuexit@163.com>
2022-03-20 20:53:38 +0800

    offer-62 圆圈中最后剩下的数字[约瑟夫环f(n,m)=(f(n-1, m) + m) % n]

--- LeetCode/Tests/offer_62.swift
+++ LeetCode/Tests/offer_62.swift
@@ -27,23 +27,55 @@ import XCTest
 
 class offer_62 {
     
-    static func lastRemaining(_ n: Int, _ m: Int) -> Int {
+    /**
+     例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+     f(n,m) n个数字,每次删除第3个元素, 最后剩下的数字
+     
+     下标:  0  1  2  3  4
+     内容:  a  b  c  d  e
+     
+     f(5,3) = 3   从a开始报数，最后能活下来的是d
+     从a开始报数，杀掉c之后，剩下4个人，接下来从d开始报数，最后能活下来的依然是d
+     以d为下标0开始报数, 4个人报数, 最终活下来的是d
+     
+     从5人变为4人，胜利者的下标由3变为0，所以f(5,3)=f(4,3) + 3
+     考虑是循环的报数的, 有可能越界: 所以: f(n,m)  = (  f(n-1, m) + m  ) % n
+第一次:
+     下标:  2  3  0  1
+     内容:  a  b  d  e
+     */
+    class Solution {
         
+        //f(n,m)  = (  f(n-1, m) + m  ) % n
+        //迭代解法:
+        /**
+         f(5,3)=( f(4,3) + 3) % 5
+         f(4,3)=( f(3,3) + 3) % 4
+         f(3,3)=( f(2,3) + 3) % 3
+         f(2,3)=( f(1,3) + 3) % 2
+         f(1,3)=0
+         */
+        func lastRemaining(_ n: Int, _ m: Int) -> Int {
+            var res = 0
+            
+            for i in 2...n {
+                res = (res + m) % i
+            }
+            
+            return res
+        }
         
-        return 10
-    }
-}
-
-class offer_62Test: XCTestCase {
-
-    let case0 = [2, 4, 6, 7, 8]
-    let case1 = [2, 7, 11, 15]
-    let case2 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-
-    func test() {
-        let result0 = TwoSum.twoSum(case0, 9)
-        XCTAssert(result0.count == 2 && case0[result0.first!] + case0[result0.last!] == 9, "case0 error")
+        //f(n,m)  = (  f(n-1, m) + m  ) % n
+        //递归解法:
+        func lastRemaining1(_ n: Int, _ m: Int) -> Int {
+            if n == 1 { return 0 }
+            return (lastRemaining(n-1, m) + m) % n
+        }
     }
     
+    func test() {
+        XCTAssert(Solution().lastRemaining(5, 3) == 3)
+        XCTAssert(Solution().lastRemaining(10, 17) == 2)
+    }
 }
 
